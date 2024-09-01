b25bdce01bdc8e9973d1a5813750d4d0fcdf85bd
Aleks <>
2024-09-02 03:20:19 +0500

    Displaying weather Data at DOM. Need to work on UI and DATA representation, need to organize some code and previous functions

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const date = document.getElementById("date");
-const city = document.getElementById("city");
-const temp = document.getElementById("temp");
-const tempImg = document.getElementById("tempImg");
-const description = document.getElementById("description");
-const tempMax = document.getElementById("tempMax");
-const tempMin = document.getElementById("tempMin");
+const date = document.querySelector("[data-weather__date]");
+const city = document.querySelector("[data-weather__city]");
+const temp = document.querySelector("[data-weather__temperature]");
+const tempImg = document.querySelector("[data-weather__icon]");
+const description = document.querySelector("[data-weather__description]");
+const tempMax = document.querySelector("[data-weather__max-temperature]");
+const tempMin = document.querySelector("[data-weather__min-temperature]");
 
 
 
-const searchBar = document.getElementById("searchBarInput");
+const searchBarInput = document.querySelector("[data-search-bar__input]");
+const searchBarList = document.querySelector("[data-search-bar__list]");
+
+
+
+
 
 
 const months = [
@@ -26,18 +31,19 @@ const months = [
   "December",
 ];
 
-let dateObj = new Date();
+let dateObj = new Date()
 let month = months[dateObj.getUTCMonth()];
-let day = dateObj.getUTCDate() - 1;
-let year = dateObj.getUTCFullYear();
+let day = dateObj.getDate();
+let year = dateObj.getFullYear();
+//console.log(dateObj)
 
-date.innerHTML = `${month}${day}, ${year}`;
+date.innerHTML = `${month} ${day}, ${year}`;
 
 const app = document.getElementById("app");
 
 // function that sends user input value to API to try to find a required city
 const sendInputDataToApi = async () => {
-  let inputValue = await searchBar.value;
+  let inputValue = await searchBarInput.value;
   console.log(`test1 send data to API${inputValue}`);
   try {
     const fetchCities = await fetch(
@@ -98,9 +104,74 @@ const analyzeDataFromApi = async (data) => {
   }
 };
 
+// function that gets weather data from API
+// needs to be reworked and completed
+const getWeather = async (data) => {
+  //convert data to int and limit numbers to 2 after point x.xx (Number().toFixed())
+  
+  
+  let  latitude = await data.latitude
+  let  longitude = await data.longitude
+  let currentCity = await data.name
+
+  let weatherApiUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,apparent_temperature,is_day,weather_code,wind_speed_10m,wind_direction_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,wind_speed_10m_max,wind_direction_10m_dominant&timezone=auto`
+  //let weatherApiUrl = `https://api.open-meteo.com/v1/dwd-icon?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,is_day,weather_code&daily=weather_code,apparent_temperature_max,apparent_temperature_min`
+  try {
+    const weatherDataFetch = await fetch(weatherApiUrl
+      ,
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+    //console.log (weatherDataFetch.status)
+
+    if (weatherDataFetch.status == 200) {
+      const weatherData = await weatherDataFetch.json();
+      const currentWeather = {
+        temperature: weatherData.current.temperature_2m,
+        dayOrNight: weatherData.current.is_day,
+        weatherCode: weatherData.current.weather_code,
+        windDirection: weatherData.current.wind_direction_10m,
+        windSpeed: weatherData.wind_speed_10m
+      }
+
+      const dailyWeather = {
+        sunrise: weatherData.daily.sunrise,
+        sunset: weatherData.daily.sunset,
+        tempMax: weatherData.daily.temperature_2m_max,
+        tempMin: weatherData.daily.temperature_2m_min,
+        time: weatherData.daily.time,
+        weatherCode: weatherData.daily.weather_code,
+        windDirection: weatherData.daily.wind_direction_10m_dominant,
+        windSpeed: weatherData.daily.wind_speed_10m_max
+
+
+      }
+   
+      console.log(`The weather for ${currentCity} is ${currentWeather.temperature}`);
+      console.log(weatherData)
+      console.log (`current temp is ${currentWeather.temperature} and the temperature for tomorrow is ${dailyWeather.tempMin[0]} - ${dailyWeather.tempMax[0]}`)
+    
+    //add data to the DOM
+    city.innerHTML = currentCity;
+    temp.innerHTML = currentWeather.temperature;
+    
+    }
+    
+    
+    
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+
 // function that removes all child elements from UL (list)
 const removeTagLi = () => {
-  let searchBarList = document.querySelector("[data-search-bar-list]");
+  let searchBarList = document.querySelector("[data-search-bar__list]");
   while (searchBarList.firstChild) {
     searchBarList.removeChild(searchBarList.firstChild);
   }
@@ -108,7 +179,7 @@ const removeTagLi = () => {
 
 // function that creates a LI element, passes API data to it and then displays it at DOM
 const createTagLiForCity = async(data,eventListener,dataExt) => {
-  let searchBarList = document.querySelector("[data-search-bar-list]");
+  let searchBarList = document.querySelector("[data-search-bar__list]");
 
   let li = document.createElement("li");
   li.innerHTML = data;
@@ -122,23 +193,23 @@ const createTagLiForCity = async(data,eventListener,dataExt) => {
 //debugger;
 
 // event listener that invokes API-related functions on input
-searchBar.addEventListener("input", ()=>{analyzeDataFromApi(sendInputDataToApi)});
+searchBarInput.addEventListener("input", ()=>{analyzeDataFromApi(sendInputDataToApi)});
 
 // function that tests Event Listener Behavior. It emulates user input
 function testEventListenerBehavior () {
 let event = new Event ('input');
-searchBar.dispatchEvent(event);
+searchBarInput.dispatchEvent(event);
 }
 
 
 // function that adds eventListener for tag <li>
 // when clicked        - get coordinates for this city
 //                     - activate getWeather function
-// then fetching the DATA from API and fill it to the DOM
+// then fetch the DATA from API and fill it to the DOM
 //clear all <li> tags
 //clear input field value when the data is fetched and displayed at the DOM
 function addEventListenerForTagLi (liTag, data){
-  liTag.addEventListener ("click", ()=>{getWeather(data)})
+  liTag.addEventListener ("click", ()=>{getWeather(data), removeTagLi()})
 
 }
 
@@ -151,37 +222,6 @@ function addEventListenerForTagLi (liTag, data){
 // -------------end
 
 
-// function that gets weather data from API
-// needs to be reworked and completed
-const getWeather = async (data) => {
-  //let testData = await data
-  //console.log("GetWeather Test " + testData)
-  //convert data to int and limit numbers to 2 after point x.xx (Number().toFixed())
-  let  latitude = await data.latitude
-  let  longitude = await data.longitude
-  let city = await data.name
-  //latitude = await data.latitude;
-  //longitude = await data.longitude;
-  try {
-    const weatherDataFetch = await fetch(
-      `https://api.open-meteo.com/v1/dwd-icon?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,is_day,weather_code&daily=weather_code,apparent_temperature_max,apparent_temperature_min`,
-      {
-        headers: {
-          Accept: "application/json",
-        },
-      }
-    );
-    const weatherData = await weatherDataFetch.json();
-    const currentWeather = await weatherData.current.temperature_2m
-    console.log(`The weather for ${city} is ${currentWeather}`);
-    console.log(weatherData)
-    //city.innerHTML;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-//getWeather();
 
 
 //create an object with data
